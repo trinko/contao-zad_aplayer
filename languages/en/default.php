4b59dbf1604bcbfc5a50022724dcde9e8e4d4fa2
trinko <trinko@tiscali.it>
2013-08-09 20:37:01 +0200

    Added English translation files

---
+++ languages/en/default.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ *
+ * Copyright (c) 2005-2013 Leo Feyer
+ *
+ * @package   zad_aplayer
+ * @author    Antonello Dessì
+ * @license   LGPL
+ * @copyright Antonello Dessì 2011-2013
+ */
+
+
+/**
+ * Miscellaneous
+ */
+$GLOBALS['TL_LANG']['CTE']['zad_aplayer'] = array('Accessible Audio/Video', 'Audio/Video player with closed captions and accessible controls');
+
+$GLOBALS['TL_LANG']['zad_aplayer']['hlp_preview'] = 'Press PLAY to see the clip';
+$GLOBALS['TL_LANG']['zad_aplayer']['hlp_play'] = 'Play';
+$GLOBALS['TL_LANG']['zad_aplayer']['hlp_pause'] = 'Pause';
+$GLOBALS['TL_LANG']['zad_aplayer']['hlp_volume'][0] = 'Mute';
+$GLOBALS['TL_LANG']['zad_aplayer']['hlp_volume'][1] = 'Low Volume';
+$GLOBALS['TL_LANG']['zad_aplayer']['hlp_volume'][2] = 'Normal Volume';
+$GLOBALS['TL_LANG']['zad_aplayer']['hlp_volume'][3] = 'High Volume';
+$GLOBALS['TL_LANG']['zad_aplayer']['hlp_cc_on'] = 'Closed Captions On';
+$GLOBALS['TL_LANG']['zad_aplayer']['hlp_cc_off'] = 'Closed Captions Off';
+$GLOBALS['TL_LANG']['zad_aplayer']['hlp_full_screen'] = 'Full Screen';
+$GLOBALS['TL_LANG']['zad_aplayer']['hlp_normal_screen'] = 'Exit Full Screen';
+$GLOBALS['TL_LANG']['zad_aplayer']['hlp_slider_tips'] = '<strong>Left/Down:</strong> 10 seconds backward<br /><strong>Right/Up:</strong> 10 seconds forward<br /><strong>Page Down:</strong> 1 minute backward<br /><strong>Page Up:</strong> 1 minute forward<br /><strong>Home:</strong> clip begin<br /><strong>End:</strong> clip end';
+$GLOBALS['TL_LANG']['zad_aplayer']['hlp_volume_tips'] = '<strong>Left/Down:</strong> decrease volume<br /><strong>Right/Up:</strong> increase volume<br /><strong>Home:</strong> no sound<br /><strong>End:</strong> max volume';
+
+$GLOBALS['TL_LANG']['zad_aplayer']['err_javascript'] = 'You have to enable JavaScript in your browser to play the clip';
+$GLOBALS['TL_LANG']['zad_aplayer']['err_download'] = 'Download the file';
+$GLOBALS['TL_LANG']['zad_aplayer']['err_flash'] = 'You have to install an updated version of Adobe Flash Player to see the clip';
+$GLOBALS['TL_LANG']['zad_aplayer']['err_flash_download'] = 'Download the last version from Adobe website';
+
---
+++ languages/en/tl_content.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ *
+ * Copyright (c) 2005-2013 Leo Feyer
+ *
+ * @package   zad_aplayer
+ * @author    Antonello Dessì
+ * @license   LGPL
+ * @copyright Antonello Dessì 2011-2013
+ */
+
+
+/**
+ * Fields
+ */
+$GLOBALS['TL_LANG']['tl_content']['zad_aplayer_playerSRC'] = array('File', 'Choose a FLV or MP3 file.');
+$GLOBALS['TL_LANG']['tl_content']['zad_aplayer_capfile'] = array('Closed Captions File', 'Choose a SubRip or Timed Text file.');
+
+
+/**
+ * Legends
+ */
+$GLOBALS['TL_LANG']['tl_content']['zad_aplayer_captions_legend'] = 'Closed Captions';
+
